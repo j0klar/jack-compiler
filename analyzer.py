a0368f2a3cd5f9832aab9623bdfe0e9a963f337d
Johannes Klar <johannesklar@outlook.com>
2026-04-27 23:11:04 +0200

    Tokenizer: handle comments & string literals

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,16 @@ from comp_engine import Comp_Engine
 import sys, os
     
 def main():
-    pass
+    path_in = sys.argv[1]
+    
+    if os.path.isfile(path_in) and path_in[-5:] == ".jack":
+        tokenizer = Tokenizer(path_in)
+        
+    elif os.path.isdir(path_in):
+        dir_in = os.listdir(path_in)
+        for file in dir_in:
+            if file[-5:] == ".jack":
+                tokenizer = Tokenizer(os.path.join(path_in, file))
     
 if __name__ == "__main__":
     main()
--- tokenizer.py
+++ tokenizer.py
@@ -1,13 +1,57 @@
 class Tokenizer:
+    """Tokenizes a single .jack-file into a stream of tokens."""
 
     def __init__(self, file):
-        pass
+        self.token_count = 0
+        self.token = ""
+        self.tokens = []
+        
+        with open(file) as stream:
+            chars = stream.read()
+            comment = False
+            token = ""
+            i = 0
+            
+            while i < len(chars):
+                if chars[i] == "/":
+                    if chars[i+1] == "/":
+                        comment = True
+                        while comment:
+                            if chars[i] == "\n":
+                                comment = False
+                            i += 1
+                        continue   
+                    elif chars[i+1] == "*":
+                        comment = True
+                        while comment:
+                            if chars[i] == "*" and chars[i+1] == "/":
+                                comment = False
+                            i += 1
+                        i += 1
+                        continue
+                        
+                elif chars[i] == "\"":
+                    i += 1
+                    while chars[i] != "\"":
+                        token += chars[i]
+                        i += 1
+                    self.tokens.append(token)
+                    i += 1
+                    continue
+            
+                if chars[i] != " " and chars[i] != "\n":
+                    print(chars[i])
+                i += 1
+            
+            print(self.tokens)
+                    
     
     def more_tokens(self):
-        pass
+        return self.token_count < len(self.tokens)
         
     def advance(self):
-        pass
+        self.token = self.tokens[self.token_count]
+        self.token_count += 1
         
     def token_type(self):
         pass
